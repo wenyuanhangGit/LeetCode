bb6fdc6bf1a5c162ce070034144593b28f85e348
温远航 <w1074673626@163.com>
2019-12-03 17:08:24 +0800

    5. 最长回文子串

---
+++ src/main/java/com/wen/leetcode/LongestPalindrome.java
@@ -0,0 +1,65 @@
+package com.wen.leetcode;
+
+import org.junit.Test;
+
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ *
+ * 输入: "cbbd"
+ * 输出: "bb"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author wen
+ * @date 2019-11-27 11:52
+ */
+public class LongestPalindrome {
+
+    @Test
+    public void test() {
+        System.out.println(longestPalindrome("babad"));
+    }
+
+    /**
+     * 最开始想的是用两个下标模拟指针，操作栈的方式来做，最后没写出来。
+     * 然后，看的官方题解。
+     * 这个用的是中心扩展算法，从思路上是比较好理解的。
+     * @param s 原始串
+     * @return 最长回文子串
+     */
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i+1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+}
